6d3f21218320a395a3c47c69b4dcbc8a4626a7df
Arthur Vieira <arthur.ev02@gmail.com>
2024-01-30 22:12:33 -0300

    Implementing admin dashboard

--- spacebio/spacebio/settings.py
+++ spacebio/spacebio/settings.py
@@ -30,15 +30,26 @@ ALLOWED_HOSTS = []
 
 # Application definition
 
-INSTALLED_APPS = [
+DJANGO_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'articles.apps.ArticlesConfig',
 ]
+
+MY_APPS = [
+    'articles.apps.ArticlesConfig',
+    'dashboard.apps.DashboardConfig',
+]
+
+THIRD_PARTY_APPS = [
+
+]
+
+
+INSTALLED_APPS = DJANGO_APPS + MY_APPS + THIRD_PARTY_APPS
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
