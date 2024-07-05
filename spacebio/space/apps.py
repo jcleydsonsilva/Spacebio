1f64ad79fe5e15ac308174411a2f2f4e70ce9470
Arthur Vieira <arthur.ev02@gmail.com>
2024-07-05 20:32:04 -0300

    Django-apschedule / implements periodic tasks

--- spacebio/space/apps.py
+++ spacebio/space/apps.py
@@ -1,37 +1,43 @@
 from django.apps import AppConfig
 
-
 class SpaceConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'space'
     
-    # def ready(self):        
-    #     from apscheduler.schedulers.background import BackgroundScheduler
-    #     from django_apscheduler.jobstores import DjangoJobStore, register_events
-    #     from django_apscheduler.jobstores import DjangoJob
-    #     import logging
+    def ready(self):
+        from apscheduler.schedulers.background import BackgroundScheduler
+        from django_apscheduler.jobstores import DjangoJob, DjangoJobStore, register_events
+        from .jobs import fetch_and_insert_news_job, fetch_and_insert_launches_job
+        import logging
+        # Configurar o logger
+        logging.basicConfig()
+        logging.getLogger('apscheduler').setLevel(logging.DEBUG)
 
-    #     from .jobs import fetch_and_insert_news_job
-        
-    #     # Configurar o logger
-    #     logging.basicConfig()
-    #     logging.getLogger('apscheduler').setLevel(logging.DEBUG)
+        # Inicializar o scheduler
+        scheduler = BackgroundScheduler()
+        scheduler.add_jobstore(DjangoJobStore(), "default")
 
-    #     # Inicializar o scheduler
-    #     scheduler = BackgroundScheduler()
-    #     scheduler.add_jobstore(DjangoJobStore(), "default")
+        # Função auxiliar para remover jobs existentes
+        def remove_job_if_exists(job_id):
+            try:
+                existing_job = DjangoJob.objects.get(id=job_id)
+                existing_job.delete()
+                print(f"Removed existing job with ID '{job_id}'.")
+            except DjangoJob.DoesNotExist:
+                pass
 
-    #     # Remover o job existente, se houver
-    #     try:
-    #         existing_job = DjangoJob.objects.get(id='fetch_and_insert_news_job')
-    #         existing_job.delete()
-    #         print(f"Removed existing job with ID 'fetch_and_insert_news_job'.")
-    #     except DjangoJob.DoesNotExist:
-    #         pass
-        
-    #     # Adicionar o job ao scheduler
-    #     scheduler.add_job(fetch_and_insert_news_job, 'interval', hours=6, id='fetch_and_insert_news_job')
+        # Remover jobs existentes
+        remove_job_if_exists('fetch_and_insert_news_job')
+        remove_job_if_exists('fetch_and_insert_launches_job')
 
-    #     # Registrar eventos e iniciar o scheduler
-    #     register_events(scheduler)
-    #     scheduler.start()
+        # Adicionar e executar jobs
+        scheduler.add_job(fetch_and_insert_news_job, 'interval', hours=6, id='fetch_and_insert_news_job')
+        scheduler.add_job(fetch_and_insert_launches_job, 'interval', hours=6, id='fetch_and_insert_launches_job')
+
+        # Registrar eventos e iniciar o scheduler
+        register_events(scheduler)
+        scheduler.start()
+
+        # Executar jobs imediatamente
+        fetch_and_insert_news_job()
+        fetch_and_insert_launches_job()
--- spacebio/space/jobs.py
+++ spacebio/space/jobs.py
@@ -1,6 +1,7 @@
 from django.core.management import call_command
-import logging
 
+def fetch_and_insert_launches_job():
+    call_command('fetch_and_insert_launches')
     
 def fetch_and_insert_news_job():
     call_command('fetch_and_insert_news')
