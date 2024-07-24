f336f4ad329bee687bf02102edb77a83f656a4e1
Arthur Vieira <arthur.ev02@gmail.com>
2024-07-24 00:24:20 -0300

    incomplete filters implementation

--- spacebio/space/filters.py
+++ spacebio/space/filters.py
@@ -5,29 +5,38 @@ from space.models import *
 class LaunchFilter(django_filters.FilterSet):
     name = django_filters.CharFilter(field_name='name', lookup_expr='icontains')
 
-    # dadndo erro porque a query coloca a hora no where, precisa comparar apenas a data e tirar a hora da condição
-    start_date = django_filters.DateFilter(field_name='window_start', method='filter_by_date')
-    def filter_by_date(self, queryset, name, value):
-        return queryset.filter(window_start__date=value)
+    # Filtros de Data
+    start_date = django_filters.DateFromToRangeFilter(field_name='window_start', label='Date Range')
 
-
-
-    # dando erro porque precisa comparar o campo status_id da tabela launches para pegar o dado da tabela status
+    # Filtro de Status
     status = django_filters.ChoiceFilter(
-        field_name='status_id__id',
+        field_name='status__id',
         choices=[
-            ('1', 'Go for Launch'),
-            ('2', 'To be Confirmed'),
-            ('3', 'To be Determined'),
-            ('4', 'Launch Successful'),
-            ('5', 'Launch Failure'),
+            (1, 'Go for Launch'),
+            (2, 'To be Confirmed'),
+            (3, 'To be Determined'),
+            (4, 'Launch Successful'),
+            (5, 'Launch Failure'),
         ],
         empty_label='Any'
     )
-    def get_status_choices():
-        return [(status.name, status.name) for status in Status.objects.all()]
-    
-    
+
+    # Filtro de Provedor de Serviços de Lançamento
+    launch_service_provider = django_filters.ModelChoiceFilter(
+        field_name='launch_service_provider',
+        queryset=LaunchServiceProvider.objects.all(),
+        label='Launch Service Provider',
+        empty_label='Any'
+    )
+
+    # Filtro de Localização de Lançamento
+    location = django_filters.ModelChoiceFilter(
+        field_name='pad__location',
+        queryset=Location.objects.all(),
+        label='Launch Location',
+        empty_label='Any'
+    )
+
     class Meta:
         model = Launch
-        fields = ['name', 'window_start', 'status']
+        fields = ['name', 'start_date', 'status', 'launch_service_provider', 'location']
